05f7cdd83a350a5416c3c6a9a1d25783b1441c66
Lorrenz Santiago <LSantiago@rightpoint.com>
2019-07-02 16:21:21 -0500

    Add components

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from '../../assests/images/logo.svg';
-import './app.scss';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from '../home/home';
+import SignInForm from '../sign-in/sign-in';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/signin'} className="nav-link">Sign In</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/signin' component={SignInForm} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
